ec17d92452080c05bbc83c8b0484810901cc4f84
Tejasvi Kondamuru <sat0049@lkqcorp.com>
2020-02-12 16:35:52 -0500

    changes to about places

--- src/Application/AboutPlaces.js
+++ src/Application/AboutPlaces.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import Events from './Events';
+//this is for about places.
 function AboutPlaces()
 {
     const gn = (thName, val)=> val==='th'? thName: val;
     return (<Events filename='aboutplaces-entries' savefilename='save-aboutplaces-entries' token='about-places'
     gridcolumns={{heading:val=>gn('Site Name', val), liner:val=>gn('Site Desc', val)}} />);
 }
-export default AboutPlaces;
+export default AboutPlaces; 
