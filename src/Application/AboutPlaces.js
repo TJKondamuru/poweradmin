885a413315434eb4c6abdf17e086a59e443cba06
TJKondamuru <tejasvi.kondamuru@gmail.com>
2020-02-18 14:11:13 -0500

    Update AboutPlaces.js
    Another simple update

--- src/Application/AboutPlaces.js
+++ src/Application/AboutPlaces.js
@@ -6,5 +6,6 @@ function AboutPlaces()
     const gn = (thName, val)=> val==='th'? thName: val;
     return (<Events filename='aboutplaces-entries' savefilename='save-aboutplaces-entries' token='about-places'
     gridcolumns={{heading:val=>gn('Site Name', val), liner:val=>gn('Site Desc', val)}} />);
+    
 }
 export default AboutPlaces; 
